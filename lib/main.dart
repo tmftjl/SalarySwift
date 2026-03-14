770cc5d9fea3cdb925235ee31bd0ac320e4725db
dggb <1607998185@qq.com>
2026-03-14 13:25:34 +0800

    改app名字为工资结算

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class SalarySwiftApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SalarySwift',
+      title: '工资结算',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
